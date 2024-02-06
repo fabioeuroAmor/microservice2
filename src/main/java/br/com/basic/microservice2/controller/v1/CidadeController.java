57589b358ffb266e003049c5323300665a2e67ad
FABIO MONTEIRO AMORIM <fmamorim_ext@grupogbi.com>
2024-02-06 14:29:51 -0300

    Subindo melhoras

--- src/main/java/br/com/basic/microservice2/controller/v1/CidadeController.java
+++ src/main/java/br/com/basic/microservice2/controller/v1/CidadeController.java
@@ -1,14 +1,16 @@
 package br.com.basic.microservice2.controller.v1;
 
 
+import br.com.basic.microservice2.json.Response;
 import br.com.basic.microservice2.service.CidadeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/v1/cidade")
+@RequestMapping(value = "/v1/cidades")
 public class CidadeController {
 
     @Autowired
@@ -17,6 +19,12 @@ public class CidadeController {
     @PostMapping(value = "/kafka/send")
    public void send(@RequestParam(value = "mensagem", required = true) String mensagem){
         cidadeService.send(mensagem);
+    }
+    @PostMapping()
+    public ResponseEntity<Response> inserirCidade(@RequestParam(value = "dcTemperatura", required = true) Double dcTemperatura, @RequestParam(value = "v10m", required = true) Double v10m,
+                                                  @RequestParam(value = "dcNome", required = true) String dcNome) {
+        return  null;
+
     }
 
 
